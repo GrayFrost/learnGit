c20f609f5dceafa53874d0d4be060fd3132a8c22
zzh <1760655799@qq.com>
2019-10-19 13:33:56 +0800

    feat(index.html): add new content

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = { 
-  extends: ["cz"] 
+  extends: ["cz"],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'docs', 'chore', 'style']],
+    'scope-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never']
+  }
 };
